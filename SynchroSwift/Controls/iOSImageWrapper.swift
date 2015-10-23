727a977ae6b26a6587a005b3d3bc814bdc26bc88
Robert Dickinson <bob.dickinson@gmail.com>
2015-10-23 01:42:51 -0700

    Image scaling and auto-sizing

--- SynchroSwift/Controls/iOSImageWrapper.swift
+++ SynchroSwift/Controls/iOSImageWrapper.swift
@@ -13,19 +13,54 @@ private var logger = Logger.getLogger("iOSImageWrapper");
 
 public class iOSImageWrapper : iOSControlWrapper
 {
+    public func toImageContentMode(value: JToken?, defaultMode: UIViewContentMode = UIViewContentMode.ScaleAspectFit) ->  UIViewContentMode
+    {
+        var mode = defaultMode;
+        let modeValue = value?.asString();
+        if (modeValue == "Stretch")
+        {
+            mode = UIViewContentMode.ScaleToFill;
+        }
+        else if (modeValue == "Fit")
+        {
+            mode = UIViewContentMode.ScaleAspectFit;
+        }
+        else if (modeValue == "Fill")
+        {
+            mode = UIViewContentMode.ScaleAspectFill;
+        }
+        return mode;
+    }
+    
     public init(parent: ControlWrapper, bindingContext: BindingContext, controlSpec:  JObject)
     {
         logger.debug("Creating image element");
         super.init(parent: parent, bindingContext: bindingContext);
         
         let image = UIImageView();
+        image.clipsToBounds = true;
+        
         self._control = image;
         
-        // !!! Image scaling
+        // Image scaling
         //
         // image.ContentMode = UIViewContentMode.ScaleToFill;     // Stretch to fill
         // image.ContentMode = UIViewContentMode.ScaleAspectFit;  // Fit preserving aspect
         // image.ContentMode = UIViewContentMode.ScaleAspectFill; // Fill preserving aspect
+        //
+        // Note: When using fit or fill there is no built-in way to control the position/alignment of the image within the view (it is always centered).
+        //       This could be accomplished by putting the UIImageView inside of another view and sizing / locating within that view based on desired
+        //       content alignment, but that's more complexity than it's worth.
+        //
+        processElementProperty(controlSpec["scale"], setValue: { (value) in
+            image.contentMode = self.toImageContentMode(value);
+            
+            // There is some scaling state maintained on changing the contentMode that can only be reset by resetting the image (after updating the content mode)
+            //
+            let theImage = image.image;
+            image.image = nil;
+            image.image = theImage;
+        });
         
         processElementDimensions(controlSpec, defaultWidth: 128, defaultHeight: 128);
         applyFrameworkElementDefaults(image);
@@ -57,6 +92,41 @@ public class iOSImageWrapper : iOSControlWrapper
                                 logger.info("Image loaded from URL: \(validUrl)");
                                 let loadedImage = UIImage(data: data!)
                                 image.image = loadedImage;
+
+                                logger.info("Image size - height: \(image.image?.size.height), width: \(image.image?.size.width)");
+                                
+                                if ((self.frameProperties.heightSpec != SizeSpec.WrapContent) && (self.frameProperties.widthSpec == SizeSpec.WrapContent))
+                                {
+                                    // Only height specified, set width based on image aspect
+                                    //
+                                    var frame = image.frame;
+                                    var size = frame.size;
+                                    
+                                    size.width = loadedImage!.size.width / loadedImage!.size.height * size.height;
+                                    
+                                    frame.size = size;
+                                    image.frame = frame;
+                                    if (image.superview != nil)
+                                    {
+                                        image.superview!.setNeedsLayout();
+                                    }
+                                }
+                                else if ((self.frameProperties.widthSpec != SizeSpec.WrapContent) && (self.frameProperties.heightSpec == SizeSpec.WrapContent))
+                                {
+                                    // Only width specified, set height based on image aspect
+                                    //
+                                    var frame = image.frame;
+                                    var size = frame.size;
+                                    
+                                    size.height = loadedImage!.size.height / loadedImage!.size.width * size.width;
+                                    
+                                    frame.size = size;
+                                    image.frame = frame;
+                                    if (image.superview != nil)
+                                    {
+                                        image.superview!.setNeedsLayout();
+                                    }
+                                }
                             }
                             else
                             {
